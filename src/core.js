7fd4af08dc98c9f81fb7a7c2f82ffa1f5feb3988
Alessandro Burato <alessandro.matteo.burato@gmail.com>
2016-08-31 15:36:50 +0200

    - updated

--- src/core.js
+++ src/core.js
@@ -15,7 +15,7 @@
 var Plotly = require('./plotly');
 
 // package version injected by `npm run preprocess`
-exports.version = '1.16.3-d9';
+exports.version = '1.16.3-d10';
 
 // plot api
 exports.plot = Plotly.plot;
