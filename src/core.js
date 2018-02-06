4e2e209c6f1d7c2baa111a5385ca1fa798ab5898
Vincenzo Scolaro <v.scolaro@iongroup.com>
2018-02-06 10:32:49 +0100

    tagging 1.33.1-ion2

--- src/core.js
+++ src/core.js
@@ -15,7 +15,6 @@ exports.version = '1.33.1-ion40';
 
 // package version injected by `npm run preprocess`
 exports.version = '1.42.5-ion48';
-
 // inject promise polyfillrequire('es6-promise').polyfill();
 
 // inject plot css
